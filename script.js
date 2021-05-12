d66c480c304724c0006adcbc2a3119b2c2c52ff5
jmetz9 <jmetz9@my.wctc.edu>
2021-05-12 15:26:14 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const NUM_BONES = 4
+
+for(let row = 0; row < NUM_BONES; row++)
+{
+    for (let col = 0; col < NUM_BONES; col++)
+    {
+        // let newSpan = $("<span onclick='dig'>");
+
+        $("p").append("<span.clickable onclick='dig'>");
+
+    }
+
+    $("p").append("<br>")
+
+}
+
+function dig(){
+
+}
